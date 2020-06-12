dc063ed197bc9eb74bb1bc073fad8ca620e89f15
lucashamaguchi <hamaguchi.lucas@gmail.com>
2020-06-11 22:24:11 -0300

    add description to model + missing done on todo interface

--- src/model/todo.ts
+++ src/model/todo.ts
@@ -4,10 +4,13 @@ import { XSchema } from "./index"
 export interface ITodo extends mongoose.Document {
   task: string;
   created_at: Date;
+  description: { type: String , required: true},
+  done: { type: Boolean, required: false, default: false },
 }
 
 const schema = new XSchema({
   task: { type: String , required: true},
+  description: { type: String , required: true},
   created_at: { type: Date, required: false, default: new Date() },
   done: { type: Boolean, required: false, default: false },
 });
