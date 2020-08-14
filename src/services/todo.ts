95b93345e557efbc9dc878fe6b8d5cf11b665a45
lucashamaguchi <hamaguchi.lucas@gmail.com>
2020-08-13 22:37:54 -0300

    remove delete override (fixed on xhelpers api) + remove "__v" field from responses

--- src/services/todo.ts
+++ src/services/todo.ts
@@ -3,18 +3,13 @@ import BaseServiceMongoose from "xhelpers-api/lib/base-service-mongoose";
 
 // mongoose
 export class TodoService extends BaseServiceMongoose<ITodo> {
+  protected sentitiveInfo: any = ["-__v"];
   constructor() {
     super(Todo);
   }
-  sentitiveInfo: any = ["-__v"];
   protected async validate(entity: ITodo, payload: ITodo): Promise<boolean> {
     const invalid = false;
     if (invalid) throw new Error("Invalid payload.");
     return Promise.resolve(true);
-  };
-  public async delete(user: any, _id: any): Promise<void> {
-    const entity = await this.Model.findById(_id).lean();
-    if (!entity) throw "Entity not found";
-    await this.Model.deleteOne({ _id });
   };
 }
