0ad9a3cd83ee605c8ae081a058719a61cb199473
lucashamaguchi <hamaguchi.lucas@gmail.com>
2020-11-24 12:52:28 -0300

    add field createdBy + lint + fix interface

--- src/model/todo.ts
+++ src/model/todo.ts
@@ -1,20 +1,22 @@
-import * as mongoose from 'mongoose';
+import * as mongoose from "mongoose";
 import { XSchema } from "./index"
 
 export interface ITodo extends mongoose.Document {
   task: string;
   created_at: Date;
-  description: { type: String , required: true},
-  done: { type: Boolean, required: false, default: false },
+  description: string;
+  done: boolean;
+  createdBy: string;
 }
 
 const schema = new XSchema({
-  task: { type: String , required: true},
-  description: { type: String , required: true},
+  task: { type: String , required: true },
+  description: { type: String , required: true },
   created_at: { type: Date, required: false, default: new Date() },
   done: { type: Boolean, required: false, default: false },
+  createdBy: { type: String , required: false },
 });
 
-schema.set('toJSON', { virtuals: true });
+schema.set("toJSON", { virtuals: true });
 
-export default mongoose.model<ITodo>('Todo', schema, 'todo');
+export default mongoose.model<ITodo>("Todo", schema, "todo");
